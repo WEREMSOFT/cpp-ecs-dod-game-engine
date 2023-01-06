e8f145fdff87d11ae62518695c9e9ac393721c27
WEREMSOFT <pablo.weremczuk@gmail.com>
2023-01-06 22:01:39 -0100

    added pool template and pool interface

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -53,8 +53,46 @@ public:
 	void RequireComponent();
 };
 
+class IPool
+{
+public:
+	virtual ~IPool() {}
+};
+
+template <typename T>
+class Pool : public IPool
+{
+private:
+	std::vector<T> data;
+
+public:
+	Pool(int size = 100) { Resize(size); }
+	virtual ~Pool() = default;
+	bool IsEmpty() const { return data.empty(); }
+	int GetSize() const { return data.size(); }
+	void Resize(int n) { data.resize(n); }
+	void Clear() { data.clear(); };
+	void Add(T object) { data.push_back(object); }
+	void Set(int index, T object) { data[index] = object; };
+	T &Get(int index) { return static_cast(T &)(data[index]); }
+	T &operator[](unsigned int index) { return data[index]; }
+};
+
 class Registry
 {
+private:
+	int numEntities = 0;
+
+	std::vector<IPool *> componentPool;
+
+public:
+	Registry() = default;
+	~Registry() = default;
+
+	Entity CreateEntity();
+	void KillEntity();
+	void AddSystem();
+	void RemoveComponent();
 };
 
 template <typename TComponent>
